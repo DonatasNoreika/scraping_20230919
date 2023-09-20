2578b13254983d67aaa13c5197eae1d47508d83b
Donatas Noreika <donoras@gmail.com>
2023-09-20 10:23:56 +0300

    Atlikta 1 užduotis: 3 patobulinimas - žaidžiama su visomis svetainės citatomis (100)

--- uzduotis2.py
+++ uzduotis2.py
@@ -3,36 +3,43 @@ from bs4 import BeautifulSoup
 from random import randint
 
 url = 'http://quotes.toscrape.com'
-r = requests.get(url)
-
-soup = BeautifulSoup(r.text, 'html.parser')
-
 quotes = []
 
-blocks = soup.find_all('div', class_='quote')
-for block in blocks:
-    quote = block.find('span', class_='text').get_text()
-    author = block.find('small', class_='author').get_text()
-    hint1 = ""
-    splitted = author.split()
-    for word in splitted:
-        if '.' not in word:
-            hint1 += f"{word[0]}."
-        else:
-            hint1 += word
-    hint2_link = block.find("a", string="(about)")['href']
-    quotes.append({"quote": quote, 'author': author, 'hint1': hint1, 'hint2_link': hint2_link})
-#
+page_counter = 1
+while True:
+    r = requests.get(f"{url}/page/{page_counter}/")
+    page_counter += 1
+    soup = BeautifulSoup(r.text, 'html.parser')
+    no_quotes_found = soup.find_all('div', class_="col-md-8")[1].next_element.get_text().strip()
+    if no_quotes_found == "No quotes found!":
+        break
+
+
+    blocks = soup.find_all('div', class_='quote')
+    for block in blocks:
+        quote = block.find('span', class_='text').get_text()
+        author = block.find('small', class_='author').get_text()
+        hint1 = ""
+        splitted = author.split()
+        for word in splitted:
+            if '.' not in word:
+                hint1 += f"{word[0]}."
+            else:
+                hint1 += word
+        hint2_link = block.find("a", string="(about)")['href']
+        quotes.append({"quote": quote, 'author': author, 'hint1': hint1, 'hint2_link': hint2_link})
+
+
+
 def get_second_hint(i):
     r = requests.get(f"{url}{quotes[i]['hint2_link']}")
     soup = BeautifulSoup(r.text, 'html.parser')
     born = soup.find("div", class_="author-details").p.get_text()
     return born
 
-print(quotes)
-
 while True:
-    i = randint(0, 9)
+    i = randint(0, len(quotes) - 1)
+    print("Index:", i)
     print(quotes[i]['quote'])
     answer1 = input("You answer: ")
     if answer1 == quotes[i]['author']:
